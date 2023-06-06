c7ef4c1175b55673f49eb4696c39bdf11ab0fd6a
stevenchungu <bed-com-07-18@cc.ac.mw>
2023-06-06 23:21:00 +0200

    modified

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,14 @@ import { PersonalityQuestionModule } from './personality-question/personality-qu
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'mysql',
-    url: process.env.DATABASE_URL,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     autoLoadEntities: true,
+    synchronize: true,
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: false,
   }),PersonalityQuestionModule,
 ],
   
