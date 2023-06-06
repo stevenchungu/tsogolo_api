9fcfa5f82649a03d91dad001b55e0d763d8e9597
stevenchungu <bed-com-07-18@cc.ac.mw>
2023-06-06 23:29:07 +0200

    add driver mysql2

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { PersonalityQuestionModule } from './personality-question/personality-qu
     autoLoadEntities: true,
     synchronize: true,
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    driver: require('mysql2'),
   }),PersonalityQuestionModule,
 ],
   
