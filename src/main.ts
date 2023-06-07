9e3f54cc66e28d86e28f50276a362f72a708a9bd
stevenchungu <bed-com-07-18@cc.ac.mw>
2023-06-07 23:53:14 +0200

    modified main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { proxyMiddleware } from 'proxy';
+import  helmet from 'helmet';
+import * as enforce from 'express-sslify';
+
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(proxyMiddleware)
-  await app.listen(process.env.PORT || 3000);
+   
+     // Enable helmet middleware for security headers
+  app.use(helmet({
+    contentSecurityPolicy: false,
+  }));
+
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  }
+  await app.listen(port);
 }
 bootstrap();
