fe25538c0dca4a2443d3defda5492e3bfdc1dccd
stevenchungu <bed-com-07-18@cc.ac.mw>
2023-06-09 00:17:36 +0200

    modified

--- src/crawling/crawling.service.ts
+++ src/crawling/crawling.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 import cheerio from 'cheerio';
-import * as request from 'request';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { JobListing } from './job-listing.entity';
@@ -15,38 +15,40 @@ export class CrawlingService {
   async crawlWebsite(): Promise<JobListing[]> {
     const url = 'https://www.alljobspo.com/malawi-jobs/';
 
-    const options = {
-      url,
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
-      },
-    };
-
-    return new Promise((resolve, reject) => {
-      request(options, (err, resp, html) => {
-        if (!err) {
-          const $ = cheerio.load(html);
-
-          // Select the job listings container and iterate over each job listing
-          const jobListings = $('article.job').map((i, element) => {
-            const title = $(element).find('h2.heading a').text().trim();
-            const sector = $(element).find('.attribute.company span.value').text().trim();
-            const location = $(element).find('.attribute.location span.value').text().trim();
-            const time = $(element).find('.attribute.location:nth-child(3) span.value').text().trim();
-            const summary = $(element).find('.summary p').text().trim();
-            const id = i;
-
-            return { title, sector, location, time, summary, id };
-          }).get();
-
-          // Save the job listings to the database
-          // const savedJobListings = await this.jobListingRepository.save(jobListings);
-
-          resolve(jobListings);
-        } else {
-          reject(err);
-        }
+    try {
+      const response = await axios.get(url, {
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+        },
       });
-    });
+
+      const html = response.data;
+      const jobListings = this.parseJobListings(html);
+
+      // Save the job listings to the database
+      // const savedJobListings = await this.jobListingRepository.save(jobListings);
+
+      return jobListings;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  }
+
+  private parseJobListings(html: string): JobListing[] {
+    const $ = cheerio.load(html);
+
+    const jobListings = $('article.job').map((i, element) => {
+      const title = $(element).find('h2.heading a').text().trim();
+      const sector = $(element).find('.attribute.company span.value').text().trim();
+      const location = $(element).find('.attribute.location span.value').text().trim();
+      const time = $(element).find('.attribute.location:nth-child(3) span.value').text().trim();
+      const summary = $(element).find('.summary p').text().trim();
+      const id = i;
+
+      return { title, sector, location, time, summary, id };
+    }).get();
+
+    return jobListings;
   }
 }
