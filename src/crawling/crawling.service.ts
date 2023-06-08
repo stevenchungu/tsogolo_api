1334bee2b88bd1b5b1681f087185e8c2b2c0d343
stevenchungu <bed-com-07-18@cc.ac.mw>
2023-06-09 00:24:51 +0200

    modified

--- src/crawling/crawling.service.ts
+++ src/crawling/crawling.service.ts
@@ -18,7 +18,7 @@ export class CrawlingService {
     try {
       const response = await axios.get(url, {
         headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'
         },
       });
 
