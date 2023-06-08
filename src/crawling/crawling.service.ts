62acfffcf2d39658c4776f108f0009e620cd8b9c
stevenchungu <bed-com-07-18@cc.ac.mw>
2023-06-09 01:15:52 +0200

    modified

--- src/crawling/crawling.service.ts
+++ src/crawling/crawling.service.ts
@@ -1,38 +1,47 @@
 // crawling.service.ts
-//'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
+
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosRequestConfig } from 'axios';
+import axios from 'axios';
 import cheerio from 'cheerio';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { JobListing } from './job-listing.entity';
+import * as request from 'request';
 
 @Injectable()
 export class CrawlingService {
   constructor(
     @InjectRepository(JobListing)
     private readonly jobListingRepository: Repository<JobListing>,
-  ) {}
+  ) { }
+
+  async fetchData(): Promise<void> {
+    const options = {
+      url: 'https://www.realmeye.com/forum/',
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+      },
+    };
+  
+    request(options, (err, resp, html) => {
+      if (!err) {
+        const gatherInformation = cheerio.load(html);
+        console.log(html);
+      }
+    });
+  }
+  
 
   async crawlWebsite(): Promise<JobListing[]> {
-    const url = 'https://www.alljobspo.com/malawi-jobs/';
-    const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'; // Replace with the desired user agent
-
-    const headers = {
-      'User-Agent': userAgent,
-      // Add any other desired headers here
-    };
-
-    const requestOptions: AxiosRequestConfig = {
-      headers,
-      // Add any other desired request options here
-    };
-
     try {
-      const response = await axios.get(url, requestOptions);
+      await this.fetchData();
+  
+      const url = 'https://www.alljobspo.com/malawi-jobs/';
+      const response = await axios.get(url);
       const html = response.data;
       const $ = cheerio.load(html);
-
+  
       // Select the job listings container and iterate over each job listing
       const jobListings = $('article.job').map((i, element) => {
         const title = $(element).find('h2.heading a').text().trim();
@@ -41,16 +50,16 @@ export class CrawlingService {
         const time = $(element).find('.attribute.location:nth-child(3) span.value').text().trim();
         const summary = $(element).find('.summary p').text().trim();
         const id = i;
-
+  
         return { title, sector, location, time, summary, id };
       }).get();
-
+  
       // Save the job listings to the database
       // const savedJobListings = await this.jobListingRepository.save(jobListings);
-
+  
       return jobListings;
     } catch (error) {
       throw new Error(error);
     }
   }
-}
+}  
