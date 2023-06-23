2b981e268fbf7cdd25a960175d7af6661dc40d12
stevenchungu <bed-com-07-18@cc.ac.mw>
2023-06-23 18:00:04 +0200

    add db configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,14 +9,16 @@ import { UserModule } from './users/user.module';
 
   imports: [TypeOrmModule.forRoot({
     type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '',
-    database: 'tsogolo',
+     host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     autoLoadEntities: false,
-    synchronize: true,
+    synchronize: false,
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    driver: require('mysql2'),
+
   }), 
    PersonalityQuestionModule,
    CrawlingModule,
